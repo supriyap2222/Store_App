12e91953a8df7dafc567c92db9c020c9a4776709
Aakash Singh <aakash.singh.as140@gmail.com>
2024-01-25 21:23:40 +0545

    handling with error navbar cartitem total

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -8,10 +8,18 @@ def counter(request):
     
     else:
         try:
-            cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            cart = list(Cart.objects.filter(cart_id=_cart_id(request)).values_list('cart_id',flat=True))
+            print(cart[0])
+            cart_items = CartItem.objects.all().filter(cart=cart[0])
+            print(cart)
+            # cart_items = CartItem.objects.values('quantity')
+            
+            print(CartItem.objects.values('quantity'))
+
             for cart_item in cart_items:
+                print(cart_item)
                 cart_count += CartItem.quantity
+                print(cart_count)
         except Cart.DoesNotExist:
             cart_count = 0
     return dict(cart_count=cart_count)
